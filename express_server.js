a74185356341bc033e0b4ec0437d91fa5f315245
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-24 00:30:50 -0500

    implemented rendering urls in table format

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
@@ -19,6 +22,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req,res)=> {
+  const templateVars = {urls:urlDatabase};
+  res.render("urls_index",templateVars);
+});
 
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
