1393531ed7549c55c2d04083ec9e3fbcce4bc498
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-24 09:00:46 -0500

    added user interface to generate new url, implemented header partial, and function to create new random string

--- express_server.js
+++ express_server.js
@@ -4,6 +4,17 @@ const PORT = 8080;
 
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let result = [];
+  let charas = "abcdefghijklmnopqrstuvwxyz0123456789"
+  for (let i = 0 ; i < 6; i++) {
+    let rand = Math.floor(Math.random() * (charas.length - 1) + 1);
+    result.push(charas[rand]);
+  }
+  return result.join('');
+}
 
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
@@ -25,6 +36,17 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req,res)=> {
   const templateVars = {urls:urlDatabase};
   res.render("urls_index",templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
