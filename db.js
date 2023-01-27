f527cc8f7a4645c8d3bdc7edc01af5ccf0b3d61e
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-26 23:03:31 -0500

    modularized database

---
+++ db.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs")
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+
+const users = {
+  userRandomID : {
+    id:"userrandomid",
+    email:"user@email.com",
+    password:"remotebranch"},
+    aJ48lW : {
+      id: "aJ48lW",
+      email : "sam@jetstream",
+      password : bcrypt.hashSync("pie",10)
+    }
+}
+
+module.exports = {urlDatabase,users};
