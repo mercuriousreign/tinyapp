d9fb543a4a8b62812315cb693e76ad85f42812a1
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-28 00:07:45 -0500

    added tester and also renamed ejs for ease of readability

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert, Assertion } = require('chai');
+
+const { getUserByEmail } = require('../helper.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.strictEqual(user,expectedUserID);
+  });
+  it('should return null when user doesnt exist', function () {
+    const user = getUserByEmail("nouser@example.com", testUsers)
+    const expectedUserID = null;
+    assert.strictEqual(user,expectedUserID);
+  });
+});
